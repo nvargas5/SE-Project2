37945294e3350c526571e9e3e5b5c4b521fdd69e
Josh Brucker <joshbrucker@gmail.com>
2019-03-23 18:36:47 -0400

    Update ShoppingCartTest.java

--- ShoppingCartTest.java
+++ ShoppingCartTest.java
@@ -1,14 +1,21 @@
 import static org.junit.Assert.*;
+
+import csc4700.CartItem;
+import csc4700.ShoppingCart;
+import csc4700.Item;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingCartTest {
 
     @Test
     public void testAddItemNull(){
-
         Item i = null;
         ShoppingCart sc = new ShoppingCart();
 
-        try{
+        try {
             sc.addItem(i);
         }
         catch(Exception e){
@@ -25,8 +32,8 @@ public class ShoppingCartTest {
         i1.setDescription("yum");
 
         sc.addItem(i1);
-        assertTrue(sc.getCartItems().contains(i1));
-
+        assertNotNull(sc.findCartItem(i1));
+        assertEquals(1, sc.findCartItem(i1).getCount());
     }
 
     @Test
@@ -39,7 +46,8 @@ public class ShoppingCartTest {
 
         sc.addItem(i1);
         sc.addItem(i1);
-        assertFalse(sc.getCartItems().contains(i1));
+        assertNotNull(sc.findCartItem(i1));
+        assertEquals(2, sc.findCartItem(i1).getCount());
     }
 
     @Test
@@ -56,12 +64,84 @@ public class ShoppingCartTest {
     }
 
     @Test
-    public void testDeleteInList(){
+    public void testDeleteInListOnlyOne(){
+        Item i1 = new Item();
+        i1.setName("apple");
 
+        ShoppingCart sc = new ShoppingCart();
+        sc.addItem(i1);
+        sc.deleteItem(i1);
 
+        assertNull(sc.findCartItem(i1));
     }
+
+    @Test
+    public void testDeleteInListMoreThanOne(){
+        Item i1 = new Item();
+        i1.setName("apple");
+
+        ShoppingCart sc = new ShoppingCart();
+        sc.addItem(i1);
+        sc.addItem(i1);
+        sc.deleteItem(i1);
+
+        assertNotNull(sc.findCartItem(i1));
+        assertEquals(1, sc.findCartItem(i1).getCount());
+    }
+
     @Test
     public void testDeleteNotInList(){
+        Item i1 = new Item();
+        i1.setName("apple");
 
+        ShoppingCart sc = new ShoppingCart();
+
+        List<CartItem> compare = new ArrayList<CartItem>(sc.getCartItems());
+        sc.deleteItem(i1);
+
+        assertArrayEquals(compare.toArray(), sc.getCartItems().toArray());
+    }
+
+    @Test
+    public void testFindCartItem() {
+        Item i1 = new Item();
+        i1.setName("apple");
+
+        Item i2 = new Item();
+        i2.setName("pear");
+
+        ShoppingCart sc = new ShoppingCart();
+        sc.addItem(i1);
+        sc.addItem(i2);
+
+        assertEquals(i1, sc.findCartItem(i1).getItem());
+    }
+
+    @Test
+    public void testFindCartItemNull() {
+        Item i1 = new Item();
+        i1.setName("apple");
+        ShoppingCart sc = new ShoppingCart();
+
+        assertNull(sc.findCartItem(i1));
+    }
+
+    @Test
+    public void testGetCartItems() {
+        Item i1 =  new Item();
+        i1.setName("apple");
+
+        Item i2 =  new Item();
+        i2.setName("pear");
+
+        List<CartItem> compare = new ArrayList<CartItem>();
+        compare.add(new CartItem(i1));
+        compare.add(new CartItem(i2));
+
+        ShoppingCart sc = new ShoppingCart();
+        sc.addItem(i1);
+        sc.addItem(i2);
+
+        assertArrayEquals(compare.toArray(), sc.getCartItems().toArray());
     }
 }
