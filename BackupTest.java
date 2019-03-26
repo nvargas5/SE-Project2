b883e947f1871e7613717954ea9b3314ece62beb
Josh Brucker <joshbrucker@gmail.com>
2019-03-26 13:46:20 -0400

    Update BackupTest.java

--- BackupTest.java
+++ BackupTest.java
@@ -1,3 +1,6 @@
+import csc4700.Backup;
+import csc4700.Item;
+import csc4700.ShoppingCart;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -5,60 +8,36 @@ import static org.junit.Assert.*;
 public class BackupTest {
 
     @Test
-    public void testSerializeShoppingCartNull(){
+    public void testSerializeCartNull() {
         ShoppingCart sc = null;
         Backup b = new Backup();
-
-        try{
-            b.serializeShoppingCart(sc);
-        }
-        catch(Exception e){
+        try {
+            assertEquals("null", b.serializeShoppingCart(sc));
+        } catch (Exception e) {
             assertTrue(e instanceof NullPointerException);
         }
     }
 
     @Test
-    public void testSerializeShoppingCart(){
+    public void testSerializeCart() {
         Item i1 = new Item();
-        Item i2 = new Item();
-
         i1.setName("apple");
-        i1.setCost(1);
         i1.setDescription("yum");
+        i1.setCost(2);
 
+        Item i2 = new Item();
         i2.setName("pear");
-        i2.setCost(2);
-        i2.setDescription("eww");
-
+        i2.setDescription("ew");
+        i2.setCost(3);
 
         ShoppingCart sc = new ShoppingCart();
-        Backup b = new Backup();
-
         sc.addItem(i1);
         sc.addItem(i2);
 
-        b.serializeShoppingCart(sc);
+        Backup b = new Backup();
 
-        StringBuffer itemLineTest = new StringBuffer();
-
-        itemLineTest.append(i1.getName());
-        itemLineTest.append(",");
-        itemLineTest.append(i1.getCost());
-        itemLineTest.append(",");
-        itemLineTest.append(i1.getDescription());
-        itemLineTest.append(",");
-        itemLineTest.append(sc.findCartItem(i1).getCount());
-        itemLineTest.append(System.getProperty("line.separator"));
-
-        itemLineTest.append(i2.getName());
-        itemLineTest.append(",");
-        itemLineTest.append(i2.getCost());
-        itemLineTest.append(",");
-        itemLineTest.append(i2.getDescription());
-        itemLineTest.append(",");
-        itemLineTest.append(sc.findCartItem(i2).getCount());
-        itemLineTest.append(System.getProperty("line.separator"));
-
+        String expected = "apple,2,yum,1" + System.getProperty("line.separator")
+                + "pear,3,ew,1" + System.getProperty("line.separator");
+        assertEquals(expected, b.serializeShoppingCart(sc));
     }
-
 }
